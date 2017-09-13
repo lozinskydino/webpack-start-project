0c3a50e03d64373c898f921fa7914730a13914fb
Dino Lozinsky <adso.menezes@gmail.com>
2017-09-13 11:05:01 -0300

    Commit Básico de projeto

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+			    test: /\.css$/,
+			    exclude: /node_modules/,
+			    loader: 'style-loader!css-loader'
+			},
+			{
+			    test: /\.scss$/,
+			    exclude: /node_modules/,
+			    loader: 'style-loader!css-loader!sass-loader'
+			}
+        ],
+    }
+};
